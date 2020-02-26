421556eb45f97353618a43377510629da2226cdb
Justin Murillo <boxmonkey13@outlook.com>
2020-02-26 01:26:23 -0700

    Finished Part 2

--- Part 2/rules.js
+++ Part 2/rules.js
@@ -74,10 +74,85 @@ When the game has not started the flag is set to false. As soon as the game star
 Once the game has finished or user has clicked on reset_play the flag must be set to false.
 */
 function game_started(){
+	// enable the move input line
 	return this.started
+
 }
+/*
+@Return integer
+@Param
+By column A1,B1,C1 -- A2,B2,C2 -- A3,B3,C3
+By row A1,A2,A3 -- B1,B2,B3 -- C1,C2,C3
+By diagonal A1,B2,C3 -- C1,B2,A3
+If board_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1], DRAW!
+// board_state = [0,1,2,3,4,5,6,7,8]
+*/
+function check_winner() {
+	// First checks for a winning pattern, then determines if pattern belongs to x or o
+	// if (board_state[0] == board_state[3] == board_state[6] || board_state[1] == board_state[4] == board_state[7] || board_state[2] == board_state[5] == board_state[8] ){
+	// 	alert("winner Column!")
+	// } else if (board_state[0] == board_state[1] == board_state[2] || board_state[3] == board_state[4] == board_state[5] ||board_state[6] == board_state[7] == board_state[8]) {
+	// 	alert("winner Row")
+	// } else if (board_state[0] == board_state[4] == board_state[8] || board_state[2] == board_state[4] == board_state[6]) {
+	// 	alert("winner diagonal")
+	// } else {
+	// 	alert("no winners detected")
+	// }
+	// check rows
+	var XorO = '';
+	if (!whose_move()){
+		XorO = "X"
+	} else {
+		XorO = "O"
+	}
 
+	if ((Number(board_state[0]) == Number(board_state[1])) && (Number(board_state[1]) == Number(board_state[2])) && Number(board_state[0]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
+	if ((Number(board_state[3]) == Number(board_state[4])) && (Number(board_state[4]) == Number(board_state[5])) && Number(board_state[3]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
+	if ((Number(board_state[6]) == Number(board_state[7])) && (Number(board_state[7]) == Number(board_state[8])) && Number(board_state[6]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
 
+	//check columns
+	if ((Number(board_state[0]) == Number(board_state[3])) && (Number(board_state[3]) == Number(board_state[6])) && Number(board_state[0]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
+	if ((Number(board_state[1]) == Number(board_state[4])) && (Number(board_state[4]) == Number(board_state[7])) && Number(board_state[1]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
+	if ((Number(board_state[2]) == Number(board_state[5])) && (Number(board_state[5]) == Number(board_state[8])) && Number(board_state[2]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
+
+	//check diags
+	if ((Number(board_state[0]) == Number(board_state[4])) && (Number(board_state[4]) == Number(board_state[8])) && Number(board_state[0]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
+	if ((Number(board_state[2]) == Number(board_state[4])) && (Number(board_state[4]) == Number(board_state[6])) && Number(board_state[2]) != -1) {
+		alert("Winner is " + XorO + "!")
+		reset_play();
+	}
+	var tempArray = [];
+	for (var i = 0; i < 9; i++) {
+		if (board_state[i] == -1) {
+			tempArray.push(-1);
+		}
+	}
+	console.log(tempArray)
+	if(!tempArray.includes(-1)){
+		alert("draw!")
+	}
+}
 /*
 TODO - Rule 1
 This is the first method you'll implement. This method is called when the Begin Play button is clicked.
@@ -91,13 +166,27 @@ The method should do all the validations as stated in rule 1.
 
 function begin_play(){
 	var player1_name = document.getElementById("player1_id");
-	var player2_name = document.getElementById("player1_id");
-//use var started
-	if (player1_name.value.length < 1 || player2_name.value.length < 1){
-		alert("please enter a name for both players!");
-	} else {
-		game_started();
+	var player2_name = document.getElementById("player2_id");
+	var button = document.getElementById("begin_play_button");
+	var reset_play_button = document.getElementById("reset_play_button");
+	reset_play_button.disabled = false;
+
+	if (player1_name.value == '' || player2_name.value == '') {
+		alert("Both player names must be filled in!");
+	} else if (player1_name.value == player2_name.value) {
+		alert("Player names cannot be identical.")
+	} else if (game_started() == true) {
+		alert("Already started. Please click \'Reset Play\' to start again.");
 		button.disabled = true;
+	} else {
+		started = true;
+		player1_name.disabled = true;
+		player2_name.disabled = true;
+		player1_name.value = player1_name.value.concat(" (X)");
+		player2_name.value = player2_name.value.concat(" (O)");
+		var turn = document.getElementById("turn_info");
+		turn.innerHTML = "Turn for: " + player1_name.value.bold();
+
 	}
 }
 
@@ -113,7 +202,46 @@ Remember to set the strated flag as false
 
 */
 function reset_play(){
+	var player1_name = document.getElementById("player1_id");
+	var player2_name = document.getElementById("player2_id");
+	var begin_play_button = document.getElementById("begin_play_button");
+	var turn = document.getElementById("turn_info");
+	var a1 = document.getElementById("A1");
+	var a2 = document.getElementById("A2");
+	var a3 = document.getElementById("A3");
+	var b1 = document.getElementById("B1");
+	var b2 = document.getElementById("B2");
+	var b3 = document.getElementById("B3");
+	var c1 = document.getElementById("C1");
+	var c2 = document.getElementById("C2");
+	var c3 = document.getElementById("C3");
+	var array = [a1,a2,a3,b1,b2,b3,c1,c2,c3];
 
+	player1_name.disabled = false;
+	player2_name.disabled = false;
+	player1_name.value = '';
+	player2_name.value = '';
+
+	if (game_started() == false) {
+		alert("Game has not been started yet. Please fill out names for Player 1 and Player 2 and click \'Begin Play\' to start again.");
+		reset_play_button.disabled = true;
+	}
+	started = false;
+	turn.innerHTML = "Game has not begun";
+	begin_play_button.disabled = false;
+
+	// Set board to default entries
+	a1.innerHTML="A1";
+	a2.innerHTML="A2";
+	a3.innerHTML="A3";
+	b1.innerHTML="B1";
+	b2.innerHTML="B2";
+	b3.innerHTML="B3";
+	c1.innerHTML="C1";
+	c2.innerHTML="C2";
+	c3.innerHTML="C3";
+	board_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+	turn = 1;
 }
 
 /*
@@ -133,7 +261,80 @@ The method should do all the things as stated in rule 2.
 
 */
 function play() {
+	// initial game state check
+	if (game_started() == false) {
+		alert("Game has not been started yet. Please fill out names for Player 1 and Player 2 and click \'Begin Play\' to start again.");
+	} else {
+		var turn = document.getElementById("turn_info");
+		var player1_name = document.getElementById("player1_id");
+		var player2_name = document.getElementById("player2_id");
+		var play_button = document.getElementById("play_button");
+		var board_input = document.getElementById("move_text_id");
+		var a1 = document.getElementById("A1");
+		var a2 = document.getElementById("A2");
+		var a3 = document.getElementById("A3");
+		var b1 = document.getElementById("B1");
+		var b2 = document.getElementById("B2");
+		var b3 = document.getElementById("B3");
+		var c1 = document.getElementById("C1");
+		var c2 = document.getElementById("C2");
+		var c3 = document.getElementById("C3");
+		var array = [a1,a2,a3,b1,b2,b3,c1,c2,c3];
+		var invalid_move = 1;
+		// checks if valid coordinates and if empty coordinates
+		for (var i = 0; i < table_ids.length; i++) {
+			if (board_input.value == table_ids[i] && board_state[i] == -1) {
 
+				// puts valid input into board
+				board_state[i] = whose_move();
+				// update board
+				if(whose_move()) {
+					//set coordinate to x
+					for (var i = 0; i < array.length; i++) {
+						// iterate through array to see where array[i] == board_input.value
+						if(array[i].innerHTML == board_input.value) {
+							array[i].innerHTML = "X";
+						}
+					}
+				} else {
+					//set coordinate to o
+					for (var i = 0; i < array.length; i++) {
+						// iterate through array to see where array[i] == board_input.value
+						if(array[i].innerHTML == board_input.value) {
+							array[i].innerHTML = "O";
+						}
+					}
+				}
+				// next turn
+				toggle_move();
+
+				// update turn on page
+				if(whose_move()) {
+					turn.innerHTML = "Turn for: " + player1_name.value.bold();
+				} else {
+					turn.innerHTML = "Turn for: " + player2_name.value.bold();
+				}
+				invalid_move = 0;
+
+
+			}
+		}
+		// no match therefore invalid input
+		if(invalid_move) {
+			var valid_move = [];
+			//scans board for empty spots
+			for (var i = 0; i < table_ids.length; i++){
+				if (board_state[i] == -1) {
+					valid_move.push(table_ids[i]);
+					console.log(valid_move);
+				}
+			}
+			alert("Invalid move. List of valid moves are: " + valid_move);
+			valid_move = [];
+		}
+		board_input.value = '';
+		check_winner();
+	}
 }
 
 /*
