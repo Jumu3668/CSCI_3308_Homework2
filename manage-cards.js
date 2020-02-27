b579f3f4efab0597bbcb8a3a447667c6b36ec6fe
Justin Murillo <boxmonkey13@outlook.com>
2020-02-26 21:58:49 -0700

    Finished

--- manage-cards.js
+++ manage-cards.js
@@ -1,12 +1,18 @@
-function addCard() {
-    var int1 = document.getElementById("interest1");
-    var int2 = document.getElementById("interest2");
-    var int3 = document.getElementById("interest3");
-    var int4 = document.getElementById("interest4");
+var counter = 0;
+function addCard(interest) {
 
-    var myInput = document.getElementById("myInput()");
 
-    myInput.onkeyup = function() {
+    // generate unique id
+    var tweet = $('<div class="card-body" id = twitter'+counter+'><img class="card-img-top" src="https://tctechcrunch2011.files.wordpress.com/2012/08/twitter-logo.jpg" alt="Card image cap"><div class="card-body"><button type="button" class="btn btn-danger btn-sm btn-circle" onclick="deleteCard(twitter'+counter+')"></button><h5 class="card-title">Tweet</h5><p class="card-text">Witty and Inspirint Tweet</p></div></div>');
+    tweet.appendTo(interest);
+    counter++;
+    console.log(counter)
 
-    }
+}
+
+function deleteCard(id) {
+    $(id).remove();
+    counter--;
+    console.log(counter)
+
 }
