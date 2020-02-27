94c054a12fa78368862dbac8abb11471ae9036c5
Justin Murillo <boxmonkey13@outlook.com>
2020-02-26 22:07:15 -0700

    Update manage-cards.js

--- manage-cards.js
+++ manage-cards.js
@@ -3,7 +3,7 @@ function addCard(interest) {
 
 
     // generate unique id
-    var tweet = $('<div class="card-body" id = twitter'+counter+'><img class="card-img-top" src="https://tctechcrunch2011.files.wordpress.com/2012/08/twitter-logo.jpg" alt="Card image cap"><div class="card-body"><button type="button" class="btn btn-danger btn-sm btn-circle" onclick="deleteCard(twitter'+counter+')"></button><h5 class="card-title">Tweet</h5><p class="card-text">Witty and Inspirint Tweet</p></div></div>');
+    var tweet = $('<div class="card-body text-right" id = twitter'+counter+'><button type="button" class="btn btn-danger btn-sm btn-circle" onclick="deleteCard(twitter'+counter+')"></button><img class="card-img-top" src="https://tctechcrunch2011.files.wordpress.com/2012/08/twitter-logo.jpg" alt="Card image cap"><div class="card-body"><h5 class="card-title">Tweet</h5><p class="card-text">Witty and Inspiring Tweet</p></div></div>');
     tweet.appendTo(interest);
     counter++;
     console.log(counter)
